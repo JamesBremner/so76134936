d75ef46e87dd192100ac982aaafe2eee96442703
JamesBremner <james@ravenspoint.com>
2023-04-29 12:42:44 -0400

    Manhatten path, no blocks

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
+#include "cRunWatch.h"
 
-class cGUI : public cStarterGUI
+std::vector<std::pair<int, int>> path(int sx, int sy, int dx, int dy)
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+
+    std::vector<std::pair<int, int>> ret;
+    ret.push_back(std::make_pair(sx, sy));
+    int deltax = dx - sx;
+    int deltay = dy - sy;
+    if (deltax)
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
-
-        show();
-        run();
+        int inc = 1;
+        if (deltax < 0)
+            inc = -1;
+        int x = sx;
+        do
+        {
+            x += inc;
+            ret.push_back(std::make_pair(x, sy));
+        } while (x != dx);
     }
-
-private:
-    wex::label &lb;
-};
+    if (deltay)
+    {
+        int inc = 1;
+        if (deltay < 0)
+            inc = -1;
+        int y = sy;
+        do
+        {
+            y += inc;
+            ret.push_back(std::make_pair(dx, y));
+        } while (y != dy);
+    }
+    return ret;
+}
 
 main()
 {
-    cGUI theGUI;
+    std::cout << "Input source location (x  y ) and destination location, e.g:\n"
+                 " 1 3 6 7\n";
+    int sx, sy, dx, dy;
+    std::cin >> sx >> sy >> dx >> dy;
+
+    std::vector<std::pair<int, int>> vpath;
+    raven::set::cRunWatch::Start();
+    {
+        raven::set::cRunWatch aWatcher("path calculation");
+        vpath = path(sx, sy, dx, dy);
+    }
+    	
+    raven::set::cRunWatch::Report();
+
+    for (auto &p : vpath)
+        std::cout << p.first << " " << p.second << "\n";
+
     return 0;
 }
