f73f51f79bb2d9f31e5d065d990271b7f66715bc
JamesBremner <james@ravenspoint.com>
2023-04-29 13:44:56 -0400

    Blocking check, try other way around

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,17 @@
 #include <algorithm>
 #include "cRunWatch.h"
 
-std::vector<std::pair<int, int>> path(int sx, int sy, int dx, int dy)
+bool isBlocked(int x, int y)
 {
+    return false;
+}
 
+std::vector<std::pair<int, int>>
+ManhattanX(int sx, int sy, int dx, int dy)
+{
     std::vector<std::pair<int, int>> ret;
     ret.push_back(std::make_pair(sx, sy));
     int deltax = dx - sx;
-    int deltay = dy - sy;
     if (deltax)
     {
         int inc = 1;
@@ -22,9 +26,20 @@ std::vector<std::pair<int, int>> path(int sx, int sy, int dx, int dy)
         do
         {
             x += inc;
+            if (isBlocked(x, sy))
+                throw std::runtime_error("blocked");
             ret.push_back(std::make_pair(x, sy));
         } while (x != dx);
     }
+    return ret;
+}
+
+std::vector<std::pair<int, int>>
+ManhattanY(int sx, int sy, int dx, int dy)
+{
+    std::vector<std::pair<int, int>> ret;
+    ret.push_back(std::make_pair(sx, sy));
+    int deltay = dy - sy;
     if (deltay)
     {
         int inc = 1;
@@ -34,10 +49,66 @@ std::vector<std::pair<int, int>> path(int sx, int sy, int dx, int dy)
         do
         {
             y += inc;
+            if (isBlocked(dx, y))
+                throw std::runtime_error("blocked");
             ret.push_back(std::make_pair(dx, y));
         } while (y != dy);
     }
     return ret;
+}
+
+std::vector<std::pair<int, int>>
+ManhattanXY(int sx, int sy, int dx, int dy)
+{
+    std::vector<std::pair<int, int>> ret;
+
+    ret = ManhattanX(sx, sy, dx, dy);
+
+    auto pathy = ManhattanY(dx, sy, dx, dy);
+
+    ret.insert(
+        ret.end(),
+        pathy.begin() + 1,
+        pathy.end());
+
+    return ret;
+}
+
+std::vector<std::pair<int, int>>
+ManhattanYX(int sx, int sy, int dx, int dy)
+{
+    std::vector<std::pair<int, int>> ret;
+
+    ret = ManhattanY(sx, sy, dx, dy);
+
+    auto pathx = ManhattanX(sx, dy, dx, dy);
+
+    ret.insert(
+        ret.end(),
+        pathx.begin() + 1,
+        pathx.end());
+
+    return ret;
+}
+
+std::vector<std::pair<int, int>>
+Manhattan(int sx, int sy, int dx, int dy)
+{
+    try {
+        return ManhattanXY(sx, sy, dx, dy);
+    }
+    catch( std::exception& e )
+    {
+        try {
+            return ManhattanYX(sx, sy, dx, dy);
+        }
+        catch(  std::exception& e )
+        {
+            ;
+        }
+    }
+    std::cout << "Blocked both ways\n";
+    exit(1);
 }
 
 main()
@@ -51,9 +122,9 @@ main()
     raven::set::cRunWatch::Start();
     {
         raven::set::cRunWatch aWatcher("path calculation");
-        vpath = path(sx, sy, dx, dy);
+        vpath = Manhattan(sx, sy, dx, dy);
     }
-    	
+
     raven::set::cRunWatch::Report();
 
     for (auto &p : vpath)
